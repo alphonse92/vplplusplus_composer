49a2d543043f961fa885b64ae5c5cc5d851360eb
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-10 20:16:43 -0500

    updating docker-compose and install script

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@ mkdir -p ./data/mysql/
 mkdir -p ./data/moodle_data/
 mkdir -p ./data/moodle/
 mkdir -p ./data/mongo
-git submodule update --init --recursive
-git submodule foreach --recursive git fetch
-git submodule foreach git merge origin master
+
+git submodule init
+git submodule add git@github.com:alphonse92/docker-moodle.git
+git submodule add git@github.com:alphonse92/vplplusplus_api.git
+git submodule add git@github.com:alphonse92/vplplusplus_client.git
+git submodule add git@github.com:alphonse92/vplplusplus_jail.git
+git submodule add git@github.com:alphonse92/vplplusplus_jail_jlib.git
+git submodule add git@github.com:alphonse92/vplplusplus_jlib.git
+git submodule update --recursive
