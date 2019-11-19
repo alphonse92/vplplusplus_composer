2096741464ca283aa15ee3570661bcf2ec0fe50b
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-11-18 21:47:34 -0500

    adding more examples

---
+++ docs/files/submission-examples/Calculadora.java
@@ -0,0 +1,20 @@
+
+public class Calculadora{
+
+  public int sumar(int i1, int i2){
+    return i1 + i2;
+  }
+
+  public int restar(int i1, int i2){
+    return i1 - i2;
+  }
+
+  public double dividir(int i1, int i2){
+    return i1 / i2;
+  }
+
+  public int multiplicar(int i1, int i2){
+    return i1 * i2;
+  }
+
+}
