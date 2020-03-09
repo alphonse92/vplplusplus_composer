b398144625f1f25ddb548b28669440dd579f46d2
Eliecer Alejandro Molina Vergel <alejandromover92@gmail.com>
2020-03-09 18:13:58 -0500

    fix

--- install.sh
+++ install.sh
@@ -5,12 +5,6 @@ mkdir -p ./data/mongo
 mkdir -p ./certs/
 
 git submodule init
-git submodule add git@github.com:alphonse92/docker-moodle.git
-git submodule add git@github.com:alphonse92/vplplusplus_api.git
-git submodule add git@github.com:alphonse92/vplplusplus_client.git
-git submodule add git@github.com:alphonse92/vplplusplus_jail.git
-git submodule add git@github.com:alphonse92/vplplusplus_jail_jlib.git
-git submodule add git@github.com:alphonse92/vplplusplus_jlib.git
 
 cd vplplusplus_api && npm install
 cd ../vplplusplus_client && npm install
